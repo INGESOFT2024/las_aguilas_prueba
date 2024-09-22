283c6b2c40917c9bc0627323dd5e7aa82b4964b4
lucasartal56 <lucdanart@gmail.com>
2024-09-22 17:14:43 -0600

    agregar ruta layout

--- views/layout.php
+++ views/layout.php
@@ -36,9 +36,9 @@
                             <li>
                                 <a class="dropdown-item nav-link text-white " href="/las_aguilas_prueba/puestos"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Añadir Puesto</a>
                             </li>
-                        
-                    
-                        
+                            <li>
+                                <a class="dropdown-item nav-link text-white " href="/las_aguilas_prueba/turnos"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Asignar Turnos</a>
+                            </li>                
                         </ul>
                     </div> 
 
